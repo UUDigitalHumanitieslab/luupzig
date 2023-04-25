08271e0aa58cb1f727bb0d95ac93a895f139bd82
Sheean Spoel <s.j.j.spoel@uu.nl>
2023-04-25 09:47:37 +0200

    Fixing tests

--- elements/src/app/view-wrapper/view-wrapper.component.spec.ts
+++ elements/src/app/view-wrapper/view-wrapper.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ViewWrapperComponent } from './view-wrapper.component';
+import { LuupzigModule } from 'luupzig';
 
 describe('ViewWrapperComponent', () => {
   let component: ViewWrapperComponent;
@@ -8,9 +9,10 @@ describe('ViewWrapperComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ViewWrapperComponent ]
+      declarations: [ViewWrapperComponent],
+      imports: [LuupzigModule]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(ViewWrapperComponent);
     component = fixture.componentInstance;
