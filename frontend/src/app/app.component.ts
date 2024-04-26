128412222e13b0d2b00cee7192abea182349f9a7
Sheean Spoel <s.j.j.spoel@uu.nl>
2024-04-26 10:20:23 +0200

    Updated to Angular 17 and Bulma 1.0.0

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+  private darkMode = new BehaviorSubject<boolean>(false);
+  darkMode$ = this.darkMode.asObservable();
+
+  constructor(@Inject(DOCUMENT) document: Document) {
+    const style = document.createElement('link');
+    style.rel = 'stylesheet';
+
+    document.head.append(style);
+
+    this.darkMode.subscribe(dark => {
+      const theme = dark ? 'dark' : 'light';
+      document.documentElement.classList.remove(dark ? 'theme-light' : 'theme-dark');
+      document.documentElement.classList.add(`theme-${theme}`);
+
+      style.href = `${theme}.css`;
+    });
+  }
+
+  toggleDark() {
+    this.darkMode.next(!this.darkMode.value);
+  }
 }
